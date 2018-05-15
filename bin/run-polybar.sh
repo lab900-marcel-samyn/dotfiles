7abd3f4908dd113124909df01108f99b829c7316
Marcel Samyn <dev@mrcl.io>
2018-05-15 17:44:53 +0200

    :bug: fix(desktop): multi-screen polybar

--- bin/run-polybar.sh
+++ bin/run-polybar.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
 
 killall polybar -q
-polybar example &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
